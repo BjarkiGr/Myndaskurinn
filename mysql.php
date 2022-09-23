768f0e2b32c49ceec9182ca116b5bae160028763
BjarkiGr <bjg56@hi.is>
2022-09-23 19:50:34 +0000

    sql fixed error message

--- mysql.php
+++ mysql.php
@@ -1,11 +1,11 @@
 <?php
 $db="myndaskurinn01";
 $mysqli = new mysqli("37.148.204.139","myndaskurinn01","UPEa7pBS@");
-if (!$con)
-  {
 
-  die('Could not connect: ' . mysqli_error());
-  }
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
 
 $mysqli -> select_db("myndaskurinn01");
 ?>
